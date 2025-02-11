63a2499a8c179daf2f7b860a9646a2a1214109ef
RTNasenbaer (school) <finn.martin@freenet.de>
2025-02-11 08:52:28 +0100

    Hanoi works

--- Hanoi/src/Hanoi.java
+++ Hanoi/src/Hanoi.java
@@ -9,24 +9,25 @@ public class Hanoi extends JFrame {
     private ArrayList<Integer> tower1 = new ArrayList<Integer>();
     private ArrayList<Integer> tower2 = new ArrayList<Integer>();
 
-    public Hanoi() {
+    public Hanoi() throws InterruptedException {
         setVisible(true);
         setSize(1920,1080);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        for (int i = 3; i > 0; i--) {
+        for (int i = 8; i > 0; i--) {
             tower0.add(i);
         }
-        hanoi(tower0.size(), tower0, tower1, tower2);
-
+        hanoi(tower0.size(), tower0, tower2, tower1);
+        repaint();
     }
 
-    private void hanoi(int towerSize, ArrayList<Integer> from, ArrayList<Integer> to, ArrayList<Integer> help) {
+    private void hanoi(int towerSize, ArrayList<Integer> from, ArrayList<Integer> to, ArrayList<Integer> help) throws InterruptedException {
 
         if (towerSize == 0) return;
         hanoi(towerSize-1, from, help, to);
         System.out.println("Nimm " + towerSize + " von " + from + " und lege zu " + to);
         to.add(from.remove(from.size()-1));
         repaint();
+        Thread.sleep(50);
         hanoi(towerSize-1, help, to, from);
 
     }
@@ -35,19 +36,12 @@ public class Hanoi extends JFrame {
     public void paint(Graphics g) {
         super.paint(g);
         g.clearRect(0,0,WIDTH,HEIGHT);
-//        g.fillRect(640, 980, 50, 50);
-        for (int i = 1; i <= tower0.size(); i++) {
-            g.fillRect(640-i*10, 980-i*15, i*20, 14);
-        }
-        for (int i = 1; i <= tower1.size(); i++) {
-            g.fillRect(2*640-i*10, 980-i*15, i*20, 14);
-        }
-        for (int i = 1; i <= tower2.size(); i++) {
-            g.fillRect(3*640-i*10, 980-i*15, i*20, 14);
-        }
+        for (int i = 0; i < tower0.size(); i++) g.fillRect(480-tower0.get(i)*10, 980-i*15, tower0.get(i)*20, 14);
+        for (int i = 0; i < tower1.size(); i++) g.fillRect(2*480-tower1.get(i)*10, 980-i*15, tower1.get(i)*20, 14);
+        for (int i = 0; i < tower2.size(); i++) g.fillRect(3*480-tower2.get(i)*10, 980-i*15, tower2.get(i)*20, 14);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         new Hanoi();
     }
 
